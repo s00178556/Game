3dd40b14b286f886747a06cba8a7cf00463ba22a
Edgaras Zavialovas <S00178556@mail.itsligo.ie>
2019-10-22 12:42:33 +0100

    Extra

--- CAExample/Player.cs
+++ CAExample/Player.cs
@@ -8,18 +8,28 @@ namespace CAExample
 {
     public class Player
     {
-        // 3 PROPERTIES CREATED
+        // 3 PROPERTIES CREATED and ID is set to READONLY (no set;)
         public int ID { get; }
         public string PlayerName { get; set; }
-        public int Score { get; set; }
+        
+        //private set; can only be accesed by the method
+        public int Score { get; private set; }
 
-        //CONSTRUCTOR CREATED FOR THE PLAYER CLASS
+        //CONSTRUCTOR CREATED FOR THE PLAYER CLASS - call lower case to distinguish
         public Player(int id, string playerName, int score)
         {
 
             ID = id;
             PlayerName = playerName;
             Score = score;
+        }
+
+        //ADD THE METHOD TO INCREASE SCORE
+
+        public void IncreaseScore(int valueToAdd)
+        {
+            if(Score < 100) //wasnt nescessary just example
+            Score += valueToAdd;
         }
 
     }
--- CAExample/Program.cs
+++ CAExample/Program.cs
@@ -8,6 +8,7 @@ namespace CAExample
 {
     class Program
     {
+        //5 PLAYERS ARE ADDED TO PROGRAM.CS
         static void Main(string[] args)
         {
 
@@ -16,6 +17,20 @@ namespace CAExample
             Player player3 = new Player(3, "Kevin", 0);
             Player player4 = new Player(4, "Conor", 0);
             Player player5 = new Player(5, "Pauline", 0);
+
+            //CREATED A LIST TYPE TO OHLD ALL THE PLAYERS
+
+            List<Player> allPlayers = new List<Player>();
+            allPlayers.Add(player1);
+            allPlayers.Add(player2);
+            allPlayers.Add(player3);
+            allPlayers.Add(player4);
+            allPlayers.Add(player5);
+
+            player1.IncreaseScore(1);
+            player2.IncreaseScore(10);
+            
+
 
         }
     }
